fb0998309166d6bdb70c6f3a66be3c934648cb70
CasTolax <cantuncer32@gmail.com>
2026-06-20 15:39:11 +0300

    refactor types.h

--- kernel/types.h
+++ kernel/types.h
@@ -1,3 +1,10 @@
+/*
+ * refactor by CasTolax 2026
+ */
+
+#ifndef TYPES_H
+#define TYPES_H
+
 typedef unsigned int uint;
 typedef unsigned short ushort;
 typedef unsigned char uchar;
@@ -8,3 +15,6 @@ typedef unsigned int uint32;
 typedef unsigned long uint64;
 
 typedef uint64 pde_t;
+
+
+#endif
